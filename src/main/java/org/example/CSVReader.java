7451b3d6340ebdbc6d5e7dbab5de3a26f30b6285
Nadezhda <nadyachery@gmail.com>
2024-06-14 00:35:27 +0300

    Добавление дженериков

--- src/main/java/org/example/CSVReader.java
+++ src/main/java/org/example/CSVReader.java
@@ -11,20 +11,16 @@ import java.util.List;
 
 public class CSVReader {
 
-    public static List<Employee> getListFromCSVFile(String filename) throws IOException {
+    public static <T> List<T> getListFromCSVFile(String filename, Class<T> type) throws IOException {
 
         try(Reader reader = new BufferedReader(new FileReader("src/main/resources/" + filename))) {
-            CsvToBean<Employee> csvReader = new CsvToBeanBuilder<Employee>(reader)
-                    .withType(Employee.class)
+            CsvToBean<T> csvReader = new CsvToBeanBuilder<T>(reader)
+                    .withType(type)
                     .withSeparator(';')
                     .withIgnoreLeadingWhiteSpace(true)
                     .withIgnoreEmptyLine(true)
                     .build();
             return csvReader.parse();
         }
-        catch (RuntimeException e){
-            e.initCause(new IOException("Can`t open file"));
-        }
-        return null;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Employee> employees = CSVReader.getListFromCSVFile("foreign_names.csv");
-        for(Employee i : employees){
-            System.out.println(i.toString());
+        try {
+            List<Employee> employees = CSVReader.getListFromCSVFile("foreign_names.csv", Employee.class);
+            for(Employee i : employees){
+                System.out.println(i.toString());
+            }
         }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
